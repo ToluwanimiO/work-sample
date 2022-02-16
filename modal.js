28b3f7500d42876c05b56d7a17438d02e482ec92
ToluwanimiO <toluwanimiosuolale@yahoo.co.uk>
2022-02-16 15:25:03 +0100

    completed all designs

--- modal.js
+++ modal.js
@@ -1,8 +1,10 @@
 // Get the modal
 var modal = document.getElementById("meals");
+var modalImage = document.getElementById("modal-image-box");
 
 // Get the button that opens the modal
 var btn = document.getElementById("check-meals");
+var btnFood = document.getElementById("view-food");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -11,15 +13,25 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
+btnFood.onclick = function() {
+  modalImage.style.display = "block";
+  modal.style.zIndex = "auto";
+}
+modalImage.onclick = function() {
+  modalImage.style.display = "none";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  modalImage.style.display = "none";
+
 }
 
 // // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+   modalImage.style.display = "none";
+
   }
 }
