6f54b233f4e65d136f17a9a60b10f256c869ac0b
ToluwanimiO <toluwanimiosuolale@yahoo.co.uk>
2022-02-17 12:59:33 +0100

    included animations on exit

--- modal.js
+++ modal.js
@@ -15,7 +15,11 @@ const hideFolderContentAnimation = anime.timeline({
   autoplay: false
 });
 hideFolderContentAnimation
-
+.add({
+  targets: "#header",
+  duration: 0, 
+  translateY: 0, 
+})
 .add({
   targets: ".modal-body",
   height: [50, 10],
@@ -40,12 +44,46 @@ hideFolderContentAnimation
   duration:300
 },"-=700")
 
+const hideFoodAnimation = anime.timeline({
+  easing: "easeOutCubic",
+  autoplay: false
+});
+hideFoodAnimation
+.add({
+  targets: '#blown-food',
+  translateY:btnFood.getBoundingClientRect().y-70,
+  translateX:btnFood.getBoundingClientRect().x-130,
+},"-=1000")
+.add({
+  targets: '#blown-food',
+  scale: 6.8,
+  // translateX:1,
+  duration:0,
+})
+.add({
+  targets: '#blown-food',
+  scale: 1,
+  // translateX:-1,
+  duration:1000,
+})
+.add({
+  targets: "#modal-image-box",
+  opacity: [1, 0],
+  duration:1000,
+  // direction:'reverse'
+},"-=200")
 
 const showFoodAnimation = anime.timeline({
   easing: "easeOutCubic",
   autoplay: false
 });
 showFoodAnimation
+.add({
+  targets: "#modal-image-box",
+  opacity: [0,1],
+  duration:0,
+  // direction:'reverse'
+})
 .add({
   targets: '#blown-food',
   translateY:btnFood.getBoundingClientRect().y-70,
@@ -55,8 +93,10 @@ showFoodAnimation
   targets: '#blown-food',
   scale: 6.8,
   translateX:1,
-  duration:1000
+  duration:1000,
+  // direction:'reverse'
 })
+
 
 const showFolderContentAnimation = anime.timeline({
   easing: "easeOutCubic",
@@ -95,20 +135,28 @@ btn.onclick = function() {
   showFolderContentAnimation.play();
 }
 btnFood.onclick = function() {
-  modalImage.style.display = "block";
   modal.style.zIndex = "1";
+  // alert('hukoo')
   showFoodAnimation.play()
+  modalImage.style.display = "block";
 }
 modalImage.onclick = function() {
   // showFolderContentAnimation.reverse();
-  modalImage.style.display = "none";
+  // modalImage.style.display = "none";
+  // alert('hu')
+  hideFoodAnimation.play()
+  setTimeout(invisibleFood,400)
   // hideFolderContentAnimation.play();
+}
+function invisibleFood(){
+  modalImage.style.display = "none";
+  hideFoodAnimation.pause()
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   // alert('hi')
   hideFolderContentAnimation.play();
-  setTimeout(invisible,500)
+  setTimeout(invisible,400)
   //
   // modalImage.style.display = "none";
 
@@ -116,13 +164,14 @@ span.onclick = function() {
 function invisible()
 {
   modal.style.display = "none";
-  window.location.reload();
+  // window.location.reload();
 }
 // // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
    modalImage.style.display = "none";
+   alert('hjk')
   //  hideFolderContentAnimation.play();
   }
 }
